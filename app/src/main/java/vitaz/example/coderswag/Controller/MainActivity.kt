566f0309407859a3b6a4f1035a34cfef5fa4b7d0
vit_az <vit.azarov@gmail.com>
2020-06-23 22:14:09 -0300

    prepare for recycler view adapter

--- app/src/main/java/vitaz/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/vitaz/example/coderswag/Controller/MainActivity.kt
@@ -2,7 +2,9 @@ package vitaz.example.coderswag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import vitaz.example.coderswag.Adapters.CategoryAdapter
 import vitaz.example.coderswag.Model.Category
@@ -19,5 +21,7 @@ class MainActivity : AppCompatActivity() {
 
         adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+
+
     }
 }
