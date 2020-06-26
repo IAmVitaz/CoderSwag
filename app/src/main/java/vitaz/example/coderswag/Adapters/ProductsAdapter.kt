482ba89f0b3afb003f708ee523fc9fe2cf3a8698
vit_az <vit.azarov@gmail.com>
2020-06-25 23:15:27 -0300

    added product adapter and setting up product showing up on ProductActivity page

---
+++ app/src/main/java/vitaz/example/coderswag/Adapters/ProductsAdapter.kt
@@ -0,0 +1,41 @@
+package vitaz.example.coderswag.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import vitaz.example.coderswag.Model.Product
+import vitaz.example.coderswag.R
+
+class ProductsAdapter (val context: Context, val product : List<Product>) : RecyclerView.Adapter<ProductsAdapter.ProductHolder>() {
+
+    inner class ProductHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val productImage = itemView?.findViewById<ImageView>(R.id.productImage)
+        val productName = itemView?.findViewById<TextView>(R.id.productName)
+        val productPrice = itemView?.findViewById<TextView>(R.id.productPrice)
+
+        fun bindProduct(product: Product, context: Context){
+            val resourceId = context.resources.getIdentifier(product.image, "drawable", context.packageName)
+            productImage.setImageResource(resourceId)
+            productName.text = product.title
+            productPrice.text = product.price
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.product_list_item, parent, false)
+        return ProductHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return product.count()
+    }
+
+    override fun onBindViewHolder(holder: ProductHolder, position: Int) {
+        holder?.bindProduct(product[position], context)
+    }
+}
--- app/src/main/java/vitaz/example/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/vitaz/example/coderswag/Controller/ProductsActivity.kt
@@ -1,16 +1,42 @@
 package vitaz.example.coderswag.Controller
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_products.*
+import vitaz.example.coderswag.Adapters.ProductsAdapter
 import vitaz.example.coderswag.R
+import vitaz.example.coderswag.Services.DataService
 import vitaz.example.coderswag.Utilities.EXTRA_CATEGORY
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 3
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+        if (screenSize > 720) {
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+        productListView.layoutManager = layoutManager
+        productListView.adapter = adapter
+
+
     }
 }
